423dbd0a15edb46977e023838e79c04307c0b92a
Minnezing <fdtochinov25@gmail.com>
2026-06-19 18:55:59 +0300

    feat(mui): add CnSS API and WS proxying

--- mui/vite.config.ts
+++ mui/vite.config.ts
@@ -1,18 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-  alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    port: 5173,
-    watch: {
-      usePolling: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    resolve: {
+    alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      port: 5173,
+      watch: {
+        usePolling: true
+      },
+      proxy: {
+        '/api/v1/ws': {
+          target: env.WS_BASE_URL,
+          ws: true,
+          changeOrigin: true,
+        },
+        '/api': {
+          target: env.API_BASE_URL,
+          changeOrigin: true,
+        }
+      }
     }
   }
 })

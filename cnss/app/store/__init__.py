ed07754918ec07788bd6eb08432e51f2cfa2f33a
Egor Kolesov <jinseisieko@gmail.com>
2026-06-19 11:55:55 +0300

    refactor: delete redundant comment

--- cnss/app/store/__init__.py
+++ cnss/app/store/__init__.py
@@ -1,8 +1,7 @@
-# app/store/__init__.py
 from .base import StateStore
 from .memory import InMemoryStateStore
 
-# Singleton instance.
-# In the future, read an ENV variable here to decide between InMemoryStateStore,
+# Singleton instance. 
+# In the future, read an ENV variable here to decide between InMemoryStateStore, 
 # RedisStateStore, or PostgresStateStore.
 state_store: StateStore = InMemoryStateStore()

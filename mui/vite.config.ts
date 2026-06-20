3144e2a8035716fd3ca12c20c82fccd94ae5b0f9
Minnezing <fdtochinov25@gmail.com>
2026-06-20 11:56:12 +0300

    style(mui): add semicolons

--- mui/vite.config.ts
+++ mui/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],

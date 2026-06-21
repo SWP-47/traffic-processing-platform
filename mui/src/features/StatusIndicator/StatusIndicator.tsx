29ab5385611bc5addecab88d3270372f602eaaad
Minnezing <fdtochinov25@gmail.com>
2026-06-21 13:38:59 +0300

    feat(mui): update StatusIndicator component

---
+++ mui/src/features/StatusIndicator/StatusIndicator.tsx
@@ -0,0 +1,36 @@
+import { useTelemetrySelector } from '@/hooks/useTelemetry';
+import styles from './StatusIndicator.module.css';
+
+function StatusIndicator() {
+  const channelId = useTelemetrySelector((tel) => tel?.data.channel_id);
+  const isActive = useTelemetrySelector((tel) => tel?.data.is_active);
+
+  const WSStatus = useTelemetrySelector((tel) => tel?.status);
+  const WSError = useTelemetrySelector((tel) => tel?.error);
+
+  let cardStyle: string = 'card';
+  let message: string = '';
+
+  if (WSStatus == 'error') {
+    cardStyle = styles.error!;
+    message = WSError!;
+  } else if (WSStatus == 'connecting') {
+    cardStyle = 'card';
+    message = 'connecting to CnSS...';
+  } else if (WSStatus == 'connected' && isActive) {
+    cardStyle = styles.active!;
+    message = 'online';
+  } else if (WSStatus == 'connected' && !isActive) {
+    cardStyle = 'card';
+    message = 'no traffic';
+  }
+
+  return (
+    <div className={`${styles.status_indicator} ${cardStyle}`}>
+      <h1 className={styles.channel_id}>{channelId || 'Select a channel'}</h1>
+      <h1 className={styles.channel_status}>{message}</h1>
+    </div>
+  );
+}
+
+export default StatusIndicator;

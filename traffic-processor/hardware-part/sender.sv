70ded457c0b956eb458d940649626e6e6fc570d7
Rena-ln <katarrrry@gmail.com>
2026-06-30 11:14:04 +0300

    feat(tp): hardware architecture was updated

---
+++ traffic-processor/hardware-part/sender.sv
@@ -0,0 +1,36 @@
+module sender (
+    input                           e_txc,
+    output                          e_gtxc,
+    output reg                      e_txen,
+    output                          e_txer,
+    output reg [7:0]                e_txd,
+
+    input                           isBlocked,
+    
+    input                           gtxc_to_send,
+    input                           txen_to_send,
+    input[7:0]                      txd_to_send
+);
+
+BUFG e1_tx_clk_buf
+(
+    .I (gtxc_to_send),
+    .O (e_gtxc)
+);
+
+assign e_txer = '0;
+
+always_ff @( posedge e_gtxc ) begin
+    if (!isBlocked) begin
+        e_txen <= txen_to_send;
+        e_txd <= txd_to_send;
+    end
+    else begin
+        e_txen <= '0;
+        e_txd <= 8'b0;
+    end
+end
+
+
+
+endmodule

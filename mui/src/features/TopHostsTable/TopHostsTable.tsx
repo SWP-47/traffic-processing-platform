ab90fe20698f0e2f64698a69efd5c9d03d66cfe1
Minnezing <fdtochinov25@gmail.com>
2026-07-02 16:53:24 +0300

    feat(mui): add top hosts table component and update dashboard

---
+++ mui/src/features/TopHostsTable/TopHostsTable.tsx
@@ -0,0 +1,60 @@
+import TopTable from '@/components/TopTable';
+import styles from './TopHostsTable.module.css';
+import { useTopHosts } from '@/hooks/useTopHosts';
+import { useEffect, useState } from 'react';
+import topHosts, { type HostsSorting, type HostsTarget } from '@/services/topHosts';
+import { useWebSocket } from '@/hooks/useWebSocket';
+import { useNavigate } from "react-router";
+
+function TopHostsTable({ mode }: { mode: 'lan' | 'wan' }) {
+  const navigate = useNavigate();
+  const [sorting, setSorting] = useState<HostsSorting>('last_seen');
+  const { connectionStatus } = useWebSocket();
+  const target = `${mode}_hosts` as HostsTarget;
+  const hosts = useTopHosts(target);
+
+  useEffect(() => {
+    if (connectionStatus !== 'connected') return;
+
+    topHosts.requestSubscription(
+      target,  // Target
+      sorting, // Sorting
+      5        // Limit
+    );
+    
+    return () => {
+      topHosts.requestUnsubscription(`${mode}_hosts`);
+    }
+  }, [connectionStatus, mode, target, sorting]);
+
+  const columns = [
+    { id: 'ip',        name: `${mode.toUpperCase()} IP`, allowSorting: false },
+    { id: 'sent',      name: 'Sent',                     allowSorting: true },
+    { id: 'received',  name: 'Received',                 allowSorting: true },
+    { id: 'last_seen', name: 'Last seen',                allowSorting: true },
+  ]
+
+  const data = hosts?.map(d => [ d.ip, d.sent_per_sec, d.received_per_sec, d.last_seen ]) ?? [];
+
+  return (
+    <div className={`card ${styles.table}`}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Top {mode.toUpperCase()} hosts</h1>
+        <p className={styles.stats}>
+          <span className={styles.stats_number}>{hosts?.length ?? 0}</span> active {mode.toUpperCase()} hosts
+        </p>
+      </div>
+      <TopTable
+        columns={columns}
+        data={data}
+
+        defaultSortColumn={sorting}
+        onSortChange={(columnId) => setSorting(columnId as HostsSorting)}
+
+        onExpanding={() => navigate('/hosts')}
+      />
+    </div>
+  );
+}
+
+export default TopHostsTable;
--- mui/src/pages/Dashboard/Dashboard.tsx
+++ mui/src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import styles from './Dashboard.module.css';
 import PacketsColumnChart from "@/features/PacketsColumnChart/PacketsColumnChart";
 import PacketsLineChart from "@/features/PacketsLineChart";
+import TopHostsTable from '@/features/TopHostsTable';
 
 function Dashboard() {
   return (
@@ -9,6 +10,10 @@ function Dashboard() {
         <div className={styles.row}>
           <PacketsColumnChart />
           <PacketsLineChart />
+        </div>
+        <div className={styles.row}>
+          <TopHostsTable mode='lan' />
+          <TopHostsTable mode='wan' />
         </div>
       </div>
     </>

babd9aa6ea8d939c8d69843abd65f89261e52262
Minnezing <fdtochinov25@gmail.com>
2026-07-11 10:02:53 +0300

    feat(mui): update getHistory method to use new verson of API

--- mui/src/services/history.ts
+++ mui/src/services/history.ts
@@ -1,14 +1,14 @@
 import apiClient from '@/api/client';
 import type { paths, components } from '@/api/schema';
 
-export type HistoryPeriod = paths["/api/v1/channel/{channel_id}/history"]["get"]["parameters"]["query"]["period"];
+export type HistoryRequest = paths["/api/v1/channel/{channel_id}/history"]["get"]["parameters"]["query"];
 export type HistoryResponse = components["schemas"]["ChannelHistoryResponse"];
 
-export async function getHistory(channelId: string, period: HistoryPeriod): Promise<HistoryResponse> {
+export async function getHistory(channelId: string, period: HistoryRequest["period_sec"], startTime?: HistoryRequest["start_time"]): Promise<HistoryResponse> {
     const { data, error } = await apiClient.GET('/api/v1/channel/{channel_id}/history', {
         params: {
             path: { channel_id: channelId },
-            query: { period: period }
+            query: { period_sec: period, start_time: startTime }
         }
     });
     

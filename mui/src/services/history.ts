54e7f2d1d22cbcf1bfeed875c84cf6f0b121e30a
Minnezing <fdtochinov25@gmail.com>
2026-07-12 14:18:50 +0300

    feat(mui): update history service to support host's history endpoint

--- mui/src/services/history.ts
+++ mui/src/services/history.ts
@@ -16,3 +16,19 @@ export async function getHistory(channelId: string, period: HistoryRequest["peri
     
     return data;
 }
+
+export type HostHistoryRequest = paths["/api/v1/channel/{channel_id}/hosts/{host_ip}/history"]["get"]["parameters"]["query"];
+export type HostHistoryResponse = components["schemas"]["HostHistoryResponse"];
+
+export async function getHostHistory(channelId: string, hostIp: string, period: HistoryRequest["period_sec"], startTime?: HistoryRequest["start_time"]): Promise<HostHistoryResponse> {
+    const { data, error } = await apiClient.GET('/api/v1/channel/{channel_id}/hosts/{host_ip}/history', {
+        params: {
+            path: { channel_id: channelId, host_ip: hostIp },
+            query: { period_sec: period, start_time: startTime }
+        }
+    });
+    
+    if (error) throw error;
+    
+    return data;
+}

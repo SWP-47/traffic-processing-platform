dcff40a964f9c47039b46665392adba278b618ba
Minnezing <fdtochinov25@gmail.com>
2026-07-14 21:19:49 +0300

    feat(mui): add api and websocket token renewal handling for seamles reauthentication

--- mui/src/api/client.ts
+++ mui/src/api/client.ts
@@ -20,8 +20,31 @@ apiClient.use({
 
         return request;
     },
-    
-    async onResponse({ response }) {
+
+    async onResponse({ request, response }) {
+        // In case of authentication error
+        if (response.status === 401) {
+            const url = new URL(request.url);
+            
+            if (url.pathname !== '/api/v1/auth/refresh' && url.pathname !== '/api/v1/auth/login') {
+                // Try to refresh token
+                const isRefreshed = await auth.handleAuthError();
+                
+                if (isRefreshed) {
+                    // Clone original query, set new token and execute
+                    const newToken = auth.getToken();
+                    const clonedRequest = new Request(request.url, {
+                        method: request.method,
+                        headers: request.headers,
+                        body: request.body,
+                    });
+                    clonedRequest.headers.set('Authorization', `Bearer ${newToken}`);
+                    
+                    return fetch(clonedRequest);
+                }
+            }
+        }
+
         if (!response.ok) {
             let errorBody;
             try {
@@ -29,8 +52,6 @@ apiClient.use({
             } catch {
                 errorBody = null;
             }
-
-            if (response.status === 401) auth.requestTokenRenewal();
             
             throw mapHttpError(response.status, errorBody);
         }

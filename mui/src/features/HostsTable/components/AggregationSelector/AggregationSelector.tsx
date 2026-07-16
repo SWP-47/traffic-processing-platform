aea99b109959a2e6a7e138eda010e47cef81f063
Minnezing <fdtochinov25@gmail.com>
2026-07-16 14:48:48 +0300

    feat(mui): update filtering system

--- mui/src/features/HostsTable/components/AggregationSelector/AggregationSelector.tsx
+++ mui/src/features/HostsTable/components/AggregationSelector/AggregationSelector.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type MouseEvent } from 'react';
+import { useEffect, useRef, useState, type MouseEvent } from 'react';
 import styles from './AggregationSelector.module.css';
 
 const SCALES = [
@@ -10,8 +10,9 @@ const SCALES = [
   { value: 2_592_000, label: '30d' },
 ];
 
-function AggregationSelector({ onTimeScaleChange }: { onTimeScaleChange: (value: number) => void }) {
-  const [timeScale, setTimeScale] = useState<number>(SCALES[0]!.value);
+function AggregationSelector({ defaultValue, onTimeScaleChange }: { onTimeScaleChange: (value: number) => void, defaultValue?: number }) {
+  const [timeScale, setTimeScale] = useState<number>(defaultValue ?? SCALES[0]!.value);
+  const buttonsContainer = useRef<HTMLDivElement>(null);
 
   // Configure time scale
   const selectScale = (event: MouseEvent<HTMLButtonElement>) => {
@@ -20,18 +21,20 @@ function AggregationSelector({ onTimeScaleChange }: { onTimeScaleChange: (value:
   };
 
   useEffect(() => {
+    if (!buttonsContainer.current) return;
+
     // Update CSS classes
-    document.querySelectorAll(`.${styles.selector}`).forEach(e => e.classList.remove(styles.active!));
-    document.querySelector(`.${styles.selector}[data-value="${timeScale}"]`)?.classList.add(styles.active!);
+    buttonsContainer.current.querySelectorAll(`.${styles.selector}`).forEach(e => e.classList.remove(styles.active!));
+    buttonsContainer.current.querySelector(`.${styles.selector}[data-value="${timeScale}"]`)?.classList.add(styles.active!);
 
     onTimeScaleChange(timeScale);
   }, [timeScale, onTimeScaleChange])
 
   return (
-    <div className={styles.selectors}>
+    <div className={styles.selectors} ref={buttonsContainer}>
       {
-        SCALES.map((scale) => (
-          <button onClick={selectScale} data-value={scale.value} className={styles.selector}>{scale.label}</button>
+        SCALES.map((scale, i) => (
+          <button key={i} onClick={selectScale} data-value={scale.value} className={styles.selector}>{scale.label}</button>
         ))
       }
     </div>

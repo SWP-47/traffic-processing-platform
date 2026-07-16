51db7b2dab4cd3f330976ea7066850df4faaa194
Minnezing <fdtochinov25@gmail.com>
2026-07-16 19:18:55 +0300

    feat(mui): add bytes to Column Chart

--- mui/src/features/HostsTable/components/HostPacketsColumnChart.tsx
+++ mui/src/features/HostsTable/components/HostPacketsColumnChart.tsx
@@ -2,17 +2,19 @@ import PacketsColumnChart from "@/features/PacketsColumnChart";
 import { useHostDetailsUpdate } from "@/hooks/useHostDetails";
 
 function HostPacketsColumnChart({ ip, aggregationPeriod }: { ip: string, aggregationPeriod: number }) {
-    const hostDetails = useHostDetailsUpdate({
-        host_ip: ip,
-        period_sec: Math.max(aggregationPeriod, 5) // If aggregationPeriod is less then 5, values are not displayed
-    });
-    
-    return (
-        <PacketsColumnChart
-            packetsIn={hostDetails?.rx_per_sec ?? 0}
-            packetsOut={hostDetails?.tx_per_sec ?? 0}
-        />
-    );
+  const hostDetails = useHostDetailsUpdate({
+    host_ip: ip,
+    period_sec: Math.max(aggregationPeriod, 5) // If aggregationPeriod is less then 5, values are not displayed
+  });
+
+  return (
+    <PacketsColumnChart
+      packetsIn={hostDetails?.rx_per_sec ?? 0}
+      packetsOut={hostDetails?.tx_per_sec ?? 0}
+      bytesIn={hostDetails?.rx_bytes_per_sec ?? 0}
+      bytesOut={hostDetails?.tx_bytes_per_sec ?? 0}
+    />
+  );
 }
 
 export default HostPacketsColumnChart;
--- mui/src/pages/Dashboard/components/RxTxColumnChar.tsx
+++ mui/src/pages/Dashboard/components/RxTxColumnChar.tsx
@@ -8,6 +8,8 @@ export function RxTxColumnChart() {
         <PacketsColumnChart
             packetsIn={update?.metrics.direction_in.packets_per_sec ?? 0}
             packetsOut={update?.metrics.direction_out.packets_per_sec ?? 0}
+            bytesIn={update?.metrics.direction_in.bytes_per_sec ?? 0}
+            bytesOut={update?.metrics.direction_out.bytes_per_sec ?? 0}
         />
     );
 }

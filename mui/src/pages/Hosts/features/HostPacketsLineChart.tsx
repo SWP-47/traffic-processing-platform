5c2b4ed8209c012c03f28165607fad36f2a052be
Minnezing <fdtochinov25@gmail.com>
2026-07-18 10:39:59 +0300

    fix(mui): update imports

--- mui/src/pages/Hosts/features/HostPacketsLineChart.tsx
+++ mui/src/pages/Hosts/features/HostPacketsLineChart.tsx
@@ -1,4 +1,4 @@
-import styles from '@/pages/Dashboard/components/LineChart/RxTxLineChart.module.css'; 
+import styles from '@/pages/Dashboard/features/LineChart/RxTxLineChart.module.css';
 import { useWebSocket } from "@/hooks/useWebSocket";
 import { ConnectionStatus } from "@/services/websocket";
 import { useMemo, useState, type MouseEvent } from "react";
@@ -8,90 +8,90 @@ import { HostDataProvider } from '@/features/PacketsLineChart/providers/HostData
 import { useUnit } from '@/contexts/UnitContext/useUnit';
 
 function HostPacketsLineChart({
-    ip, 
-    timeScale
+  ip,
+  timeScale
 }: { ip: string, timeScale: number }) {
 
-    const { params, connectionStatus } = useWebSocket();
-    const { unit } = useUnit();
+  const { params, connectionStatus } = useWebSocket();
+  const { unit } = useUnit();
 
-    const [showReceived, setShowReceived] = useState(true);
-    const [showSent, setShowSent] = useState(true);
+  const [showReceived, setShowReceived] = useState(true);
+  const [showSent, setShowSent] = useState(true);
 
-    const id = params?.channel_id;
+  const id = params?.channel_id;
 
-    const selectedSeries = useMemo<PacketsLineChartOptions["selectedSeries"]>(() => {
-        if (unit === 'bytes') {
-            return {
-                Received_bytes: showReceived,
-                Sent_bytes: showSent,
-                Received_pkts: false,
-                Sent_pkts: false
-            };
-        }
-        return {
-            Received_bytes: false,
-            Sent_bytes: false,
-            Received_pkts: showReceived,
-            Sent_pkts: showSent
-        };
-    }, [unit, showReceived, showSent]);
-
-    const provider = useMemo(() => {
-        if (!id) {
-            return null;
-        }
-        return new HostDataProvider({
-            channelId: id,
-            windowSec: timeScale,
-            hostIp: ip
-        });
-    }, [id, timeScale, ip]);
-
-    const toggleLegend = (event: MouseEvent<HTMLSpanElement>) => {
-        const series = (event.target as HTMLElement).getAttribute("data-series");
-        
-        if (series === "Received") {
-            setShowReceived(prev => !prev);
-        } else if (series === "Sent") {
-            setShowSent(prev => !prev);
-        }
+  const selectedSeries = useMemo<PacketsLineChartOptions["selectedSeries"]>(() => {
+    if (unit === 'bytes') {
+      return {
+        Received_bytes: showReceived,
+        Sent_bytes: showSent,
+        Received_pkts: false,
+        Sent_pkts: false
+      };
+    }
+    return {
+      Received_bytes: false,
+      Sent_bytes: false,
+      Received_pkts: showReceived,
+      Sent_pkts: showSent
     };
+  }, [unit, showReceived, showSent]);
 
-    return (
-        <div className={`${styles.component} card ${(connectionStatus !== ConnectionStatus.Connected) && styles.inactive}`}>
-            <div className={styles.header}>
-                <div className={styles.left}>
-                    <h1>RX/TX Rate over time</h1>
-                    <div className={styles.legend}>
-                        <span 
-                            onClick={toggleLegend} 
-                            data-series="Received"
-                            className={!showReceived ? styles.inactive : ''}
-                        >
-                            Received
-                        </span>
-                        <span 
-                            onClick={toggleLegend} 
-                            data-series="Sent"
-                            className={!showSent ? styles.inactive : ''}
-                        >
-                            Sent
-                        </span>
-                    </div>
-                </div>
-            </div>
-            {
-                provider &&
-                <PacketsLineChart
-                    dataProvider={provider}
-                    timeScale={timeScale}
-                    selectedSeries={selectedSeries}
-                    unit={unit}
-                />
-            }
+  const provider = useMemo(() => {
+    if (!id) {
+      return null;
+    }
+    return new HostDataProvider({
+      channelId: id,
+      windowSec: timeScale,
+      hostIp: ip
+    });
+  }, [id, timeScale, ip]);
+
+  const toggleLegend = (event: MouseEvent<HTMLSpanElement>) => {
+    const series = (event.target as HTMLElement).getAttribute("data-series");
+
+    if (series === "Received") {
+      setShowReceived(prev => !prev);
+    } else if (series === "Sent") {
+      setShowSent(prev => !prev);
+    }
+  };
+
+  return (
+    <div className={`${styles.component} card ${(connectionStatus !== ConnectionStatus.Connected) && styles.inactive}`}>
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <h1>RX/TX Rate over time</h1>
+          <div className={styles.legend}>
+            <span
+              onClick={toggleLegend}
+              data-series="Received"
+              className={!showReceived ? styles.inactive : ''}
+            >
+              Received
+            </span>
+            <span
+              onClick={toggleLegend}
+              data-series="Sent"
+              className={!showSent ? styles.inactive : ''}
+            >
+              Sent
+            </span>
+          </div>
         </div>
-    );
+      </div>
+      {
+        provider &&
+        <PacketsLineChart
+          dataProvider={provider}
+          timeScale={timeScale}
+          selectedSeries={selectedSeries}
+          unit={unit}
+        />
+      }
+    </div>
+  );
 }
 
 export default HostPacketsLineChart;

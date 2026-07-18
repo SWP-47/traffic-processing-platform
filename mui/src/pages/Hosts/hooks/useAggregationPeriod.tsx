52e133d8b1ef51aea3be4382fb3a1eea9bc2368b
Minnezing <fdtochinov25@gmail.com>
2026-07-18 11:20:35 +0300

    feat(mui): add support of bucket-interval endpoint

--- mui/src/pages/Hosts/hooks/useAggregationPeriod.tsx
+++ mui/src/pages/Hosts/hooks/useAggregationPeriod.tsx
@@ -1,5 +1,5 @@
 import { useWebSocket } from "@/hooks/useWebSocket";
-import { getHistory } from "@/services/history";
+import { getBucketInterval } from "@/services/history";
 import { useEffect, useState } from "react";
 
 function useAggregationPeriod(timeScale: number) {
@@ -8,12 +8,11 @@ function useAggregationPeriod(timeScale: number) {
     const { params } = useWebSocket();
     const channelId = params?.channel_id;
 
-    // Effect to get aggregationWindow from history response.
     useEffect(() => {
         if (!channelId || !timeScale) return;
 
-        getHistory(channelId, timeScale).then(response => {
-        setAggregationWindow(response.interval_sec);
+        getBucketInterval(timeScale).then(response => {
+            setAggregationWindow(response.interval_sec);
         });
     }, [channelId, timeScale]);
 
